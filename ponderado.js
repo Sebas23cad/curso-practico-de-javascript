1023f4dc9fcf1ea5008de097fde4a7d1e3e28967
Sebastian Cadena <panchito23cuid@outlook.com>
2022-03-16 10:09:14 -0500

    Terminamos el ultimo caso de los promedios.

---
+++ ponderado.js
@@ -0,0 +1,48 @@
+//Codigo de las notas.
+
+const notes = [
+    {
+        course: "Educacion Fisica",
+        note: 10,
+        credit: 2,
+    },
+    {
+        course: "Programacion",
+        note: 8,
+        credit: 5,
+    },
+    {
+        course: "Fisica",
+        note: 7,
+        credit: 5,
+    },
+    {
+        course: "Finanzas personales",
+        note: 10,
+        credit: 6,
+    },
+];
+
+const notesWithCredit = notes.map(function (noteObject) {
+    return noteObject.note * noteObject.credit;
+});
+
+const sumOfNotesWithCredit = notesWithCredit.reduce(
+    function (sum = 0, newVal) {
+        return sum + newVal;
+    }
+);
+
+const credits = notes.map(function (noteObject) {
+    return noteObject.credit;
+});
+
+const sumOfCredits = credits.reduce(
+    function (sum = 0, newVal) {
+        return sum + newVal;
+    }
+);
+
+const promedioPonderadoNotasConCredito = sumOfNotesWithCredit / sumOfCredits;
+
+console.log(promedioPonderadoNotasConCredito);
